ddcc3d2fc0a5ce309e3019a8906d048d9a34d49f
David Wasserman <davidw@grandconsult.com>
2023-04-04 22:50:08 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by David Wasserman on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
